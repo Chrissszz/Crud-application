e59c5a3a23d535ff062b008b4b323592dcd33111
Chrissszz <c.barrett228@yahoo.com>
2023-04-26 12:41:02 -0500

    mongodb

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const morgan = require('morgan')
 const bodyparser = require("body-parser");
 const path = require('path');
 
+const connectDB = require('./server/database/connection');
+
 const app = express();
 
 dotenv.config({path :'config.env'})
@@ -12,6 +14,9 @@ const PORT = process.env.PORT || 8080
 
 //log requests
 app.use(morgan('tiny'));
+
+//mongodb connection
+connectDB();
 
 //parse request to body-parser
 app.use(bodyparser.urlencoded({extended:true}));
@@ -24,12 +29,7 @@ app.set("view engine","ejs")
 app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
 app.use('/img',express.static(path.resolve(__dirname,"assets/img")))
 app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
-
-app.get('/',(req,res)=>{
-    res.render('index')
-})
-app.get('/add-user',(req,res)=>{
-    res.render('add_user')
-})
+//load routers
+app.use('/', require('./server/routes/router'))
 
 app.listen(PORT,()=> {console.log(`Server is running on http//localhost:${PORT}`)})
