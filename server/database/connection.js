c21e5d5671d3e0fc6f1d5907730431cf46e766e0
Chrissszz <c.barrett228@yahoo.com>
2023-04-26 23:57:34 -0500

    updates

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,11 +3,9 @@ const mongoose = require('mongoose');
 const connectDB = async()=>{
     try{
         //mongodb connection string
-        const con = await mongoose.connect(process.env.Mongo_URI, {
+        const con = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true
         })
         console.log(`MongoDB Connected: ${con.connection.host}`)
     }catch(err){
